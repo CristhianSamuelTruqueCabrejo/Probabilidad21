93551f1e71cd9bd8d88a91ccb5f89be1a746112c
CristhianSamuelTruqueCabrejo <crtruque@gmail.com>
2024-05-28 11:33:37 -0500

    hola

--- Simulaciones_prob_valores_esperados/simulacion_var_aleatoria_bernoulli.py
+++ Simulaciones_prob_valores_esperados/simulacion_var_aleatoria_bernoulli.py
@@ -1,9 +1,24 @@
-import random
+import time
+
+def generador_numeradores_aleatorios():
+    multiplicador  = 31
+    sumando = 17
+    norma = 1351
+
+
+    semilla = time.perf_counter_ns()
+    random  = 0
+
+
+    semilla = (multiplicador * semilla + sumando) % norma
+    random = semilla / norma
+
+    return random
 
 def simular_bernoulli(p, max_iter):
     suma = 0
     for _ in range(max_iter):
-        rnd = random.random()  # Genera un número aleatorio RN D ∈ (0,1)
+        rnd = generador_numeradores_aleatorios()  # Genera un número aleatorio RN D ∈ (0,1)
         if rnd < p:
             suma += 1
     valor_esperado = suma / max_iter
